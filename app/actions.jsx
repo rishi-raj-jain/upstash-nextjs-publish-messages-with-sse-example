22881f6ec341c96ae4b00bf82b532ea751e0fb89
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-03-08 14:15:33 +0530

    fmt

--- app/actions.jsx
+++ app/actions.jsx
@@ -10,5 +10,12 @@ export async function publishNotification(formData) {
   const redis = Redis.fromEnv()
   const headersList = headers()
   const country = headersList.get('x-vercel-ip-country')
-  await redis.publish('posts', JSON.stringify({ country, date: new Date().toString(), message: formData.get('message') }))
+  await redis.publish(
+    'posts',
+    JSON.stringify({
+      country,
+      date: new Date().toString(),
+      message: formData.get('message'),
+    }),
+  )
 }
